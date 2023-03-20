578ade6cf7e990bdb870251cbb7e0f1b6b6aa75e
Valentina <balendi.art@gmail.com>
2023-03-20 12:25:49 +0100

    navigation

--- List/ContentView.swift
+++ List/ContentView.swift
@@ -9,44 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Weather")
-                .font(.title)
-                .fontWeight(.heavy)
-                .padding()
-            ForEach(climas) { climat in
-                HStack(){
-                    Image(systemName: climat.climaIcon)
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                        .foregroundColor(.black)
-                    Text(climat.clima)
-                        .font(.body)
-                        .fontWeight(.bold)
+        NavigationStack {
+            VStack(alignment: .leading) {
+                List(climas) { climat in
+                    NavigationLink(destination: {
+                        Meteo()
+                    }, label: {
+                        HStack(){
+                            Image(systemName: climat.climaIcon)
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.black)
+                            Text(climat.clima)
+                                .font(.body)
+                                .fontWeight(.bold)
+                        }
+                        .padding()
+                    })
                 }
-                .padding()
-                Divider()
             }
+            .navigationBarTitle("Weather")
         }
     }
-}
-struct Weather: Identifiable {
-    var id = UUID()
-    var clima: String
-    var climaIcon: String
-}
-
-var climas = [
-    Weather(clima: "Sun", climaIcon: "sun.max.fill"),
-    Weather(clima: "Clouds", climaIcon: "cloud.fill"),
-    Weather(clima: "Storm", climaIcon: "cloud.bolt"),
-    Weather(clima: "Moon", climaIcon: "moon.fill"),
-    Weather(clima: "Snow", climaIcon: "snowflake"),
-    Weather(clima: "Tornado", climaIcon: "tornado")
-]
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct Weather: Identifiable {
+        var id = UUID()
+        var clima: String
+        var climaIcon: String
+    }
+    
+    var climas = [
+        Weather(clima: "Sun", climaIcon: "sun.max.fill"),
+        Weather(clima: "Clouds", climaIcon: "cloud.fill"),
+        Weather(clima: "Storm", climaIcon: "cloud.bolt"),
+        Weather(clima: "Moon", climaIcon: "moon.fill"),
+        Weather(clima: "Snow", climaIcon: "snowflake"),
+        Weather(clima: "Tornado", climaIcon: "tornado")
+    ]
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
---
+++ List/Meteo.swift
@@ -0,0 +1,22 @@
+//
+//  Meteo.swift
+//  List
+//
+//  Created by Apprenant 18 on 20/03/2023.
+//
+
+import SwiftUI
+
+struct Meteo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+struct Lista {
+
+}
+struct Meteo_Previews: PreviewProvider {
+    static var previews: some View {
+        Meteo()
+    }
+}
