0964c7cf45817b502ce16f092e5b066d5da25e3a
Valentina <balendi.art@gmail.com>
2023-03-20 09:27:28 +0100

    Initial Commit

---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by Apprenant 18 on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
