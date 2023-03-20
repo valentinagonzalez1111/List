e7f81c4e3d12abb390202d00cb9277b62faafa5f
Valentina <balendi.art@gmail.com>
2023-03-20 10:28:16 +0100

    lista dinica clima

--- List/ContentView.swift
+++ List/ContentView.swift
@@ -9,15 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Weather")
+                .font(.title)
+                .fontWeight(.heavy)
+                .padding()
+            ForEach(climas) { climat in
+                HStack(){
+                    Image(systemName: climat.climaIcon)
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.black)
+                    Text(climat.clima)
+                        .font(.body)
+                        .fontWeight(.bold)
+                }
+                .padding()
+                Divider()
+            }
         }
-        .padding()
     }
 }
+struct Weather: Identifiable {
+    var id = UUID()
+    var clima: String
+    var climaIcon: String
+}
+
+var climas = [
+    Weather(clima: "Sun", climaIcon: "sun.max.fill"),
+    Weather(clima: "Clouds", climaIcon: "cloud.fill"),
+    Weather(clima: "Storm", climaIcon: "cloud.bolt"),
+    Weather(clima: "Moon", climaIcon: "moon.fill"),
+    Weather(clima: "Snow", climaIcon: "snowflake"),
+    Weather(clima: "Tornado", climaIcon: "tornado")
+]
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
